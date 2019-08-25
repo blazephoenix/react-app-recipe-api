f4662eeb670c72b08e90997b8fb4ae176423ffec
Blaze <16274366+Blazephoenix@users.noreply.github.com>
2019-08-25 21:32:37 +0530

    updated javascript and fonts

--- src/recipe.js
+++ src/recipe.js
@@ -5,6 +5,7 @@ const Recipe = ({title,calories,image, ingredients, url}) => {
     return(
         <div className={style.recipe}>
             <a href={url} className="recipe__url" target="_blank" rel="noopener noreferrer"><h1>{title}</h1></a>
+            <small><strong>Calorie count:</strong> {calories.toFixed(2)}</small>
             <img src={image} alt="" className="recipe__image"/>
         </div>
     )
